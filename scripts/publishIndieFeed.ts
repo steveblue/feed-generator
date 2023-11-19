991f7dc0aaafb476f0859da6fc702ac34b358cde
Stephen Belovarich <steveblue@gmail.com>
2023-11-19 13:47:39 -0800

    fix: remove try catch

--- scripts/publishIndieFeed.ts
+++ scripts/publishIndieFeed.ts
@@ -47,14 +47,6 @@ const run = async () => {
   })
   await agent.login({ identifier: handle, password })
 
-  try {
-    await agent.api.app.bsky.feed.describeFeedGenerator()
-  } catch (err) {
-    throw new Error(
-      'The bluesky server is not ready to accept published custom feeds yet',
-    )
-  }
-
   let avatarRef: BlobRef | undefined
   if (avatar) {
     let encoding: string
